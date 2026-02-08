80b638dec84a50ebdbdca8726f29467766fddeb4
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-02-08 15:36:41 +0530

    `Added Google GenAI functionality and image/video generation capabilities`

--- index.js
+++ index.js
@@ -29,19 +29,96 @@
 // console.log(res.response.text());
 
 
+// ==========================google geminial ===========================
+
+
 import { GoogleGenAI } from "@google/genai";
 import dotenv from 'dotenv';
+import { readFileSync, write, writeFileSync } from "fs";
 dotenv.config();
 
-const genAI = new GoogleGenAI({apiKey: process.env.GEN_KEY});
+const genAI = new GoogleGenAI({ apiKey: process.env.GEN_KEY });
 
-async function aiAnswer(){
-    const response = await genAI.models.generateContent({
-        model: "gemini-2.5-flash",
-        contents : "give me 5 fruits name"
+// async function aiAnswer(){
+//     const response = await genAI.models.generateContent({
+//         model: "gemini-2.5-flash",
+//         contents : "what is love?",
+//         config : {
+//             systemInstruction: "answeer must be 1 word",
+//         }
+
+//     })
+//     console.log(response.text);
+// }
+// aiAnswer();
+
+// =============================================
+
+// read image from image.js
+
+// async function aiAnswer (){
+//     const imageData = readFileSync("Badge.png");
+//     const response = await genAI.models.generateContent({
+//         model: "gemini-2.5-flash",
+//         contents : [
+//             {
+//                 inlineData : {
+//                     mimeType: "image/png",
+//                     data: imageData.toString("base64") ,
+//                 },
+//             },
+//             {text : "read text from image "}
+//         ]
+//     })
+//     console.log(response.text);
+// }
+
+// aiAnswer();
+
+
+
+// ===================image generate ===================
+
+// async function aiAnswer (){  
+//     const response = await genAI.models.generateImages({
+//         model: "imagen-4.0-generate-001",
+//        prompt : "a cat sitting on the bed",
+//        config: {
+//       numberOfImages: 1,
+//     },
+//     })
+//     const image = response.generatedImages[0].image.imageBytes;
+//     const buffer = Buffer.from(image , "base64");
+//     writeFileSync("cat.png" , buffer);
+// }
+// aiAnswer();
+
+
+
+// ====================video generate======
+
+
+async function aiAnswer() {
+    const operation = await genAI.models.generateImages({
+        model: "veo-3.1-generate-001",
+        prompt: "a dog running on road",
 
     })
-    console.log(response.text);
+    while (!operation.dome) {
+    console.log("Waiting for video generation to complete...")
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    operation = await genAI.operations.getVideosOperation({
+        operation: operation,
+    });
+}
+
+
+// Download the generated video.
+genAI.files.download({
+    file: operation.response.generatedVideos[0].video,
+    downloadPath: "dialogue_example.mp4",
+});
+console.log(`Generated video saved to dialogue_example.mp4`);
 }
 
 aiAnswer();
